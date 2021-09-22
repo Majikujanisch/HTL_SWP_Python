009ca48dd183faa658a014637d1f33c7f68f098f
Majikujanisch <karottennase123@gmail.com>
2021-09-22 18:22:11 +0200

    finished Numbergame finished Chatbot

--- 23-09-21/Ubungen.py
+++ 23-09-21/Ubungen.py
@@ -9,3 +9,45 @@ number = random.randint(1,100)
 durchgange = 0
 print('Rate die Zahl:')
 while durchgange < 7:
+    userinput = input("Zahl: ")
+    if int(userinput) > number:
+        print("Gesuchte Zahl ist kleiner!")
+    if int(userinput) < number:
+        print("Gesuchte Zahl ist größer!")
+    if int(userinput) == number:
+        print("GEWONNEN!")
+        durchgange = 8
+    durchgange = durchgange + 1
+#Chatbot
+zufallsantworten = ["AJO", "Wia den des?", "Klingt nice", "BUT WHY????"]
+
+reaktionsantworten = {"hallo": "SERVAAAS",
+                      "geht": "Wie solls an bot scho gehn",
+                      "essen": "Erdbeerkäääääääse"
+                      }
+
+print("Willkommen beim tyrolian Chatbot")
+print("Wos geits zu schnacken?")
+print("Wenn koan bock mehr hasch: 'bye' eintippen")
+print("")
+
+nutzereingabe = ""
+while nutzereingabe != "bye":
+    nutzereingabe = ""
+    while nutzereingabe == "":
+        nutzereingabe = input("Ihre Frage/Antwort: ")
+
+    nutzereingabe = nutzereingabe.lower()
+    nutzerwoerter = nutzereingabe.split()
+
+    intelligenteAntworten = False
+    for einzelwoerter in nutzerwoerter:
+        if einzelwoerter in reaktionsantworten:
+            print(reaktionsantworten[einzelwoerter])
+            intelligenteAntworten = True
+    if intelligenteAntworten == False:
+        print(random.choice(zufallsantworten))
+
+    print("")
+
+print("Nagstes mol lassesch oba a bissl geld do ge? aloa zahlt sich des da nitta")
